7bba40f493e1369af2715b01e057f4d2ae5e9232
Jeffrey Scott <valenpendragon@gmail.com>
2023-02-04 17:21:07 -0500

    Add success label. Build event loop with proper exit options.

--- main.py
+++ main.py
@@ -4,41 +4,60 @@ import pandas
 
 SUPPORTED_FORMATS = ['xls', 'xlsx']
 
-sg.theme("DarkPurple")
+sg.theme("Black")
 
 file_choice_label = sg.Text("Select desired spreadsheet workbook: ")
-file_choice_input = sg.Input()
+file_choice_input = sg.Input(key="file_choice")
 file_choice_button = sg.FileBrowse("Choose", key="filepath")
 
-table_choice_label = sg.Text("Enter the name of the desired table as it "
-                              "appears on the tab in the workbook:")
+table_choice_label1 = sg.Text("Enter the name of the desired table as it appears")
+table_choice_label2 = sg.Text("on the tab in the workbook:")
 table_choice_input = sg.InputText(tooltip="table name", key="table")
 
 output_dir_label = sg.Text("Pick the desired destination directory:")
-output_dir_input = sg.Input()
+output_dir_input = sg.Input(key="folder_choice")
 output_dir_button = sg.FolderBrowse("Choose", key="dest_folder")
 
 output_file_label = sg.Text("Enter the name of the text file output:")
 output_file_choice = sg.InputText(default_text="output",
                                   key="filename")
 
+success_label = sg.Text(key="completed", text_color="white")
+
 convert_button = sg.Button("Convert Table")
+quit_button = sg.Button("Quit", key="quit")
 
 layout_col1 = [[file_choice_label],
                [file_choice_input],
-               [file_choice_button]]
+               [file_choice_button],
+               [table_choice_label1],
+               [table_choice_label2],
+               [table_choice_input]]
 
-layout_col2 = [[output_file_label],
+layout_col2 = [[output_dir_label],
                [output_dir_input],
-               [output_dir_button]]
+               [output_dir_button],
+               [output_file_label],
+               [output_file_choice],
+               [convert_button, quit_button]]
 
 col1 = sg.Column(layout=layout_col1)
 col2 = sg.Column(layout=layout_col2)
 
 window = sg.Window("Table Converter",
                    layout=[[col1, col2],
-                           [table_choice_label],
-                           [table_choice_input, convert_button]])
+                           [success_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    window["folder_choice"].update(value=values["dest_folder"])
+    window["file_choice"].update(value=values["filepath"])
+    match event:
+        case sg.WIN_CLOSED:
+            break
+        case "quit":
+            break
+
+
 window.close()
